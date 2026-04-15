06dcf14ccd26d6f1de5cc81bd9e9b7e8d0a201b9
VolodymyrDziu <credo-planers-0i@icloud.com>
2026-04-15 14:02:36 +0300

    Initial Commit

---
+++ SnowTrails/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnowTrails
+//
+//  Created by Volodymyr Dziubenko on 15.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnowTrails/SnowTrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowTrailsApp.swift
+//  SnowTrails
+//
+//  Created by Volodymyr Dziubenko on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SnowTrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
